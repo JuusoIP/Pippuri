52547d1523b14dbbb55bbd8b3eff728875a9f130
Petri Hautamäki <petri.hautamaki@hotmail.com>
2021-11-12 10:27:17 +0200

    Choregraph testi

---
+++ Pete/Choregraphe_projects/first_test/first_test.pml
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="first_test" format_version="4">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+        <BehaviorDescription name="behavior" src="test_behavior" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs />
+    <Resources>
+        <File name="test_movement" src="test_movement.pmt" />
+    </Resources>
+    <Topics />
+    <IgnoredPaths />
+    <Translations auto-fill="en_US">
+        <Translation name="translation_en_US" src="translations/translation_en_US.ts" language="en_US" />
+        <Translation name="translation_fi_FI" src="translations/translation_fi_FI.ts" language="fi_FI" />
+    </Translations>
+</Package>
---
+++ Pete/Choregraphe_projects/first_test/translations/translation_en_US.ts
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Terve</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Terve</translation>
+        </message>
+    </context>
+    <context>
+        <name>test_behavior/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Ugazaga</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Ugazaga</translation>
+        </message>
+    </context>
+</TS>
