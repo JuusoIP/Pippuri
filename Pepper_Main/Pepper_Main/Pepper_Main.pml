8db3c28f4469a2cbf7652c881530c27dd45d488c
Petri Hautamäki <petri.hautamaki@hotmail.com>
2021-12-14 12:28:06 +0200

    Ready dialogia muokattu

---
+++ Pepper_Main/Pepper_Main/Pepper_Main.pml
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="Pepper_Main" format_version="4">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+        <BehaviorDescription name="behavior" src="greeting" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs>
+        <Dialog name="HelloWorld" src="HelloWorld/HelloWorld.dlg" />
+        <Dialog name="ExampleDialog" src="behavior_1/ExampleDialog/ExampleDialog.dlg" />
+        <Dialog name="vahvistus" src="vahvistus/vahvistus.dlg" />
+        <Dialog name="tervehtiminen" src="tervehtiminen/tervehtiminen.dlg" />
+        <Dialog name="Entertain" src="Entertain/Entertain.dlg" />
+        <Dialog name="Package" src="Package/Package.dlg" />
+        <Dialog name="Guidance" src="Guidance/Guidance.dlg" />
+        <Dialog name="Confirm" src="Confirm/Confirm.dlg" />
+        <Dialog name="After_entertainment" src="After_entertainment/After_entertainment.dlg" />
+        <Dialog name="ready" src="ready/ready.dlg" />
+    </Dialogs>
+    <Resources>
+        <File name="swiftswords_ext" src="behavior_1/swiftswords_ext.mp3" />
+        <File name="taichimove" src="behavior_1/taichimove.pmt" />
+        <File name="epicsax" src="behavior_1/epicsax.ogg" />
+        <File name="hemulin nauru" src="hemulin nauru.mp3" />
+        <File name="tilutus" src="tilutus.mp3" />
+        <File name="choice_sentences" src="behavior_1/Aldebaran/choice_sentences.xml" />
+        <File name="confirm" src="html/pictures/confirm.jpg" />
+        <File name="entertainment" src="html/pictures/entertainment.jpg" />
+        <File name="follow_me" src="html/pictures/follow_me.jpg" />
+        <File name="scan_qr" src="html/pictures/scan_qr.jpg" />
+        <File name="thank_you" src="html/pictures/thank_you.jpg" />
+        <File name="welcome" src="html/pictures/welcome.jpg" />
+        <File name="to_desk" src="to_desk.pmt" />
+        <File name="to_home" src="to_home.pmt" />
+    </Resources>
+    <Topics>
+        <Topic name="HelloWorld_enu" src="HelloWorld/HelloWorld_enu.top" topicName="HelloWorld" language="en_US" />
+        <Topic name="ExampleDialog_enu" src="behavior_1/ExampleDialog/ExampleDialog_enu.top" topicName="ExampleDialog" language="en_US" />
+        <Topic name="vahvistus_enu" src="vahvistus/vahvistus_enu.top" topicName="vahvistus" language="en_US" />
+        <Topic name="tervehtiminen_enu" src="tervehtiminen/tervehtiminen_enu.top" topicName="tervehtiminen" language="en_US" />
+        <Topic name="Entertain_enu" src="Entertain/Entertain_enu.top" topicName="Entertain" language="en_US" />
+        <Topic name="Package_enu" src="Package/Package_enu.top" topicName="Package" language="en_US" />
+        <Topic name="Guidance_enu" src="Guidance/Guidance_enu.top" topicName="Guidance" language="en_US" />
+        <Topic name="Confirm_enu" src="Confirm/Confirm_enu.top" topicName="Confirm" language="en_US" />
+        <Topic name="After_entertainment_enu" src="After_entertainment/After_entertainment_enu.top" topicName="After_entertainment" language="en_US" />
+        <Topic name="ready_enu" src="ready/ready_enu.top" topicName="ready" language="en_US" />
+    </Topics>
+    <IgnoredPaths />
+    <Translations auto-fill="en_US">
+        <Translation name="translation_en_US" src="translations/translation_en_US.ts" language="en_US" />
+    </Translations>
+</Package>
---
+++ Pepper_Main/Pepper_Main/translations/translation_en_US.ts
@@ -0,0 +1,138 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/At the desk/Entertainment/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Because he couldn't see that well</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Because he couldn't see that well</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/At the desk/Entertainment/Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Why did the blind man fall into the well</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Why did the blind man fall into the well</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/At the desk/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Why did the blind man fall into the well</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Why did the blind man fall into the well</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/At the desk/Say (1)</name>
+        <message>
+            <source>Because he couldn't see that well</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Because he couldn't see that well</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/At the desk/Say (2)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Let me know when you are ready</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Let me know when you are ready</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Entertainment/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Because he couldn't see that well</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Because he couldn't see that well</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Entertainment/Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Why did the blind man fall into the well</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Why did the blind man fall into the well</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Face Detection/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Package/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Are you ready to receive your package?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Are you ready to receive your package?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Because he couldn't see that well</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Because he couldn't see that well</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Why did the blind man fall into the well</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Why did the blind man fall into the well</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (2)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Are you ready to receive your package?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Are you ready to receive your package?</translation>
+        </message>
+    </context>
+</TS>
