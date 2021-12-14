6c348dd60ead956aa273480b1c4038dc21033ed9
Petri Hautamäki <petri.hautamaki@hotmail.com>
2021-12-14 16:00:31 +0200

    Dialogien parantelua

--- Pepper_Main/Pepper_Main/translations/translation_en_US.ts
+++ Pepper_Main/Pepper_Main/translations/translation_en_US.ts
@@ -28,6 +28,15 @@
         </message>
     </context>
     <context>
+        <name>behavior_1/behavior.xar:/At the desk/Help_say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I need help. Carry me pack to my post.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I need help. Carry me pack to my post.</translation>
+        </message>
+    </context>
+    <context>
         <name>behavior_1/behavior.xar:/At the desk/Say</name>
         <message>
             <source>Hello</source>
